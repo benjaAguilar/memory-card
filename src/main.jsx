7a9ead75e9378626310236641a9d72f7152eb89d
Benja Aguilar <benjaaguilar02@gmail.com>
2024-06-08 20:01:01 -0300

    added dom content to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Header from "./header.jsx";
+import CardsGame from "./cardsGame.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <Header />
+    <CardsGame />
+  </React.StrictMode>
 );
