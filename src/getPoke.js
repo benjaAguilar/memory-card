63607f97baee1c5d9e8a49e9d8499a51c5992e7f
Benja Aguilar <benjaaguilar02@gmail.com>
2024-06-08 17:41:31 -0300

    feat: api call to get the pokemons
    this have an array of pokemons and a function that
    makes an api call returns nam and img and can handle errors

---
+++ src/getPoke.js
@@ -0,0 +1,37 @@
+export const pokemonNames = [
+  "pikachu",
+  "charmander",
+  "bulbasaur",
+  "squirtle",
+  "jigglypuff",
+  "meowth",
+  "psyduck",
+  "snorlax",
+  "gengar",
+  "eevee",
+  "mewtwo",
+  "charizard",
+  "magikarp",
+  "machamp",
+  "gyarados",
+];
+
+// get and return pokemon name and img from api
+export default async function getPokemons(name) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    const pokemon = await response.json();
+
+    return {
+      name: pokemon.name,
+      img: pokemon.sprites.front_default,
+    };
+  } catch (e) {
+    console.log("failed to fetch: " + e);
+  }
+}
